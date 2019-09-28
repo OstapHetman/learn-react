0baffa2fc694d2b3b62379c45db61e613a0669ec
OstapHetman <prickly4528@gmail.com>
2019-09-28 20:22:24 +0300

    added routes

---
+++ github-finder/src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, necessitatibus.</p>
+      <p>Version: 1.0.0</p>
+    </Fragment>
+  )
+}
+
+export default About
