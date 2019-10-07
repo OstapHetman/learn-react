6ff0ec9ad66008eee6cc4e2a42b59f957455bd5d
OstapHetman <prickly4528@gmail.com>
2019-10-07 21:33:15 +0300

    start working with featured rooms

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,19 +1,21 @@
-import React, {Fragment} from 'react'
+import React, { Fragment } from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
 import Services from '../components/Services'
+import FeaturedRooms from '../components/FeaturedRooms'
 
 export default function Home() {
     return (
-    <Fragment>
-        <Hero>
-            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-                <Link to="/rooms" className="btn btn-primary">our rooms</Link>
-            </Banner>
-        </Hero>
-        <Services />
-    </Fragment>
+        <Fragment>
+            <Hero>
+                <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                    <Link to="/rooms" className="btn btn-primary">our rooms</Link>
+                </Banner>
+            </Hero>
+            <Services />
+            <FeaturedRooms />
+        </Fragment>
     )
 }
 
