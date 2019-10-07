7630f3f1fb2d7a69fea95f22481f63cfc4a2f2c1
OstapHetman <prickly4528@gmail.com>
2019-10-07 18:22:15 +0300

    implemented services component

--- resort/src/pages/Home.js
+++ resort/src/pages/Home.js
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, {Fragment} from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import { Link } from 'react-router-dom';
+import Services from '../components/Services'
 
 export default function Home() {
-    return <Hero>
-        <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
-            <Link to="/rooms" className="btn btn-primary">our rooms</Link>
-        </Banner>
-    </Hero>
+    return (
+    <Fragment>
+        <Hero>
+            <Banner title="luxurious rooms" subtitle="deluxe rooms starting at $299">
+                <Link to="/rooms" className="btn btn-primary">our rooms</Link>
+            </Banner>
+        </Hero>
+        <Services />
+    </Fragment>
+    )
 }
 
