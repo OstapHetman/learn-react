1ac70af1c4ad73b4ad3a55faf2b458b96db6e1fa
OstapHetman <prickly4528@gmail.com>
2019-10-08 10:28:54 +0300

    added room list component

---
+++ resort/src/components/RoomsFilter.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function RoomsFlter() {
+    return (
+        <div>
+            hello from room ifilter
+        </div>
+    )
+}
--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -1,16 +1,21 @@
 import React from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner'
+import RoomsContainer from '../components/RoomsContainer'
 import {Link} from 'react-router-dom'
 
 const Rooms = () => {
-    return <Hero hero="roomsHero">
+    return <>
+        <Hero hero="roomsHero">
             <Banner title="our rooms">
                 <Link to="/" className="btn-primary">
                     return home
                 </Link>
             </Banner>
         </Hero>
+        <RoomsContainer/>
+        </>
+
 }
 
 export default Rooms
