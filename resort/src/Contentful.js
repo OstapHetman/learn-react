4b27c5ef9a3140326c91174162000a652e833212
OstapHetman <prickly4528@gmail.com>
2019-10-09 10:00:10 +0300

    ednpoint

--- resort/src/Contentful.js
+++ resort/src/Contentful.js
@@ -1,6 +1,6 @@
 import {createClient} from 'contentful'
 
 export default createClient ({
-    space: '39sba4pw0nly',
-    accessToken: "_-zwBuodjGEHmz4xj9OyNb8iIoN9gPF60mme4JlJ65c"
+    space: process.env.REACT_APP_API_SPACE,
+    accessToken: process.env.REACT_APP_API_TOKEN
 })
