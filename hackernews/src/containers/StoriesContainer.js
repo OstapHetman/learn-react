c1e8b57971224c9dc5c60dea2ec2c2de783bcbec
OstapHetman <prickly4528@gmail.com>
2019-10-11 16:14:11 +0300

    start with unfinityScroll

--- hackernews/src/containers/StoriesContainer.js
+++ hackernews/src/containers/StoriesContainer.js
@@ -2,9 +2,11 @@ import React, {useEffect, useState} from 'react';
 import { getStoryId } from '../services/hnApi'
 import { Story } from '../components/Story'
 import { GlobalStyle, StoriesContainerWrapper } from '../styles/StoriesContainerStyles';
+import { useInfiniteScroll } from '../hooks/useInfiniteScrol'
 
 export const StoriesContainer = () => {
     const [storyId, setStoryIds] = useState([]);
+    const { count } = useInfiniteScroll();
   
     useEffect(() => {
       getStoryId().then(data => setStoryIds(data))
@@ -15,7 +17,7 @@ export const StoriesContainer = () => {
         <GlobalStyle />
         <StoriesContainerWrapper data-test-id="stories-container">
           <h1>Hacker News Stories</h1>
-          {storyId.map(storyId => <Story key={storyId} storyId={storyId}/>)}
+          {storyId.slice(0, count).map(storyId => <Story key={storyId} storyId={storyId}/>)}
         </StoriesContainerWrapper>
       </>
     )
