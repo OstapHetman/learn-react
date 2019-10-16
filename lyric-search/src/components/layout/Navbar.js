f809e3ade0755e507931239f8546162b10b8648b
OstapHetman <prickly4528@gmail.com>
2019-10-16 16:59:52 +0300

    work with top 10 tracks

---
+++ lyric-search/src/components/layout/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-dark bg-dark mb-5 justify-content-center">
+            <span className="navbar-brand mb-0">
+                <h2>LyricFinder</h2>
+            </span>
+        </nav>
+    )
+}
+
+export default Navbar
