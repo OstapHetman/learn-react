049a6f2114caabac0b13e88d0b35e63209273995
OstapHetman <prickly4528@gmail.com>
2019-10-18 08:55:40 +0300

    routing

--- chat-app/src/index.js
+++ chat-app/src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import LoginComponent from './login/Login';
+import SignupComponent from './signup/Signup';
+import DashboardComponent from './dashboard/Dashboard';
 
 const firebase = require('firebase')
 require('firebase/firestore');
@@ -16,7 +20,17 @@ firebase.initializeApp({
     appId: "1:942125287434:web:ca374ec5446ce887a3b7f3"
 });
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div id="routing-container">
+            <Route path="/signin" component={LoginComponent} />
+            <Route path="/signup" component={SignupComponent} />
+            <Route path="/dashboard" component={DashboardComponent} />
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
